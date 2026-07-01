94bed69394ec908f84779048c8a31839d918d015
Aman <aman.raibole.30072003@gmail.com>
2026-07-01 22:23:26 +0530

    added something

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,18 @@ import Login from './components/Login'
 import SignUp from './components/SignUp'
 import NavigationBar from './components/NavigationBar'
 import './App.css'
+import UserSection from './views/UserSection'
 
 function App() {
   return (
     <BrowserRouter>
       <NavigationBar />
       <Routes>
+        
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/UserSection" element={<UserSection />} />
       </Routes>
     </BrowserRouter>
   )
